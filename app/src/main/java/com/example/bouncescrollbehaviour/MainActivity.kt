09c27e758c3927da35610437fddc80cbe1282e01
Ever <ever.solis@elaniin.com>
2019-11-26 09:41:16 -0600

    Bounce scroll fixed

--- app/src/main/java/com/example/bouncescrollbehaviour/MainActivity.kt
+++ app/src/main/java/com/example/bouncescrollbehaviour/MainActivity.kt
@@ -22,14 +22,20 @@ class MainActivity : AppCompatActivity() {
 
             })
        // bouncingRecycler.setHasFixedSize(true)
-     //   bouncingRecycler.configureRecyclerGrid(4)
+    //    bouncingRecycler.configureRecyclerGrid(4)
       //  bouncingRecycler.adapter =  adapter
 
     }
 
-    fun androidx.recyclerview.widget.RecyclerView.configureRecyclerGrid(elementPerRow: Int = 3): androidx.recyclerview.widget.RecyclerView {
+   private fun androidx.recyclerview.widget.RecyclerView.configureRecyclerGrid(elementPerRow: Int = 3): androidx.recyclerview.widget.RecyclerView {
         itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
         layoutManager = androidx.recyclerview.widget.GridLayoutManager(context,elementPerRow)
+        return this
+    }
+
+   private fun androidx.recyclerview.widget.RecyclerView.configureRecycler(): androidx.recyclerview.widget.RecyclerView {
+        itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
+        layoutManager = androidx.recyclerview.widget.LinearLayoutManager(context, androidx.recyclerview.widget.RecyclerView.HORIZONTAL, false)
         return this
     }
 }
--- mylibrary/src/main/java/com/ever777/bouncingscroll/BouncingNestedScrollView.kt
+++ mylibrary/src/main/java/com/ever777/bouncingscroll/BouncingNestedScrollView.kt
@@ -1,7 +1,11 @@
 package com.ever777.bouncingscroll
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
+import android.graphics.Point
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -15,99 +19,196 @@ class BouncingNestedScrollView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : NestedScrollView(context, attrs, defStyleAttr) {
-
     private var isOverScrolling = false
+    private var isOverScrollingVertical = true
     private var oldYMove = 0f
+    private var oldXMove = 0f
+    private var screenWidth: Int = 0
+    private var screenHeight: Int = 0
 
 
     init {
+        val display = (getContext() as Activity).windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        screenWidth = size.x
+        screenHeight = size.y
         configureScroll()
         this.overScrollMode = 2
+
     }
 
     @SuppressLint("ClickableViewAccessibility")
     private fun configureScroll() {
         this.setOnTouchListener { v, event ->
 
-
             val rawY: Float
+            val rawX: Float
             val location = intArrayOf(0, 0)
             v.getLocationOnScreen(location)
             rawY = event.rawY + location[1]
-
-
+            rawX = event.rawX + location[0]
 
             if (event.action == MotionEvent.ACTION_DOWN) {
                 oldYMove = rawY
-
+                oldXMove = rawX
             }
 
             if (event.action == MotionEvent.ACTION_MOVE) {
-                if (!this.canScrollVertically(1)) {
-                    for (i in 0 until this.childCount) {
-                        val view: View = this.getChildAt(i)
-                        val delta = oldYMove - rawY
-                        if (delta > 0) {
-                            isOverScrolling = true
-                            if (view.translationY > -25f && oldYMove != rawY) {
-                                view.translationY -= 0.85f
+                // horizontal scroll right to left
+                if (this.canScrollHorizontally(1) || this.canScrollHorizontally(-1)) {
+                    if (!this.canScrollHorizontally(1)) {
+                        isOverScrollingVertical = false
+                        for (i in 0 until this.childCount) {
+                            val view: View = this.getChildAt(i)
+                            val delta = oldXMove - rawX
+                            if (delta > 0) {
+                                isOverScrolling = true
+                                if (oldXMove != rawX) {
+                                    view.translationX  = (delta * 0.50f) * -1
+                                }
                             }
                         }
+                        return@setOnTouchListener false
+                    }
+                    // horizontal scroll left to right
+                    if (!this.canScrollHorizontally(-1)) {
+                        isOverScrollingVertical = false
+                        for (i in 0 until this.childCount) {
+                            val view: View = this.getChildAt(i)
+                            val delta = oldXMove - rawX
+                            if (delta < 0) {
+                                isOverScrolling = true
+                                if (oldXMove != rawX) {
+                                    view.translationX  = (delta * 0.50f) * -1
+                                }
+                            }
+                        }
+                        return@setOnTouchListener false
                     }
 
-                    //scroll up to down
-                } else if (!this.canScrollVertically(-1)) {
-                    for (i in 0 until this.childCount) {
-                        val view: View = this.getChildAt(i)
-                        val delta = rawY - oldYMove
-                        if (delta > 0) {
-                            isOverScrolling = true
-                            if (view.translationY < 25f && oldYMove != rawY) {
-                                view.translationY += 0.85f
+                }
+
+                //verticalMovement down to up
+                if (this.canScrollVertically(1) || this.canScrollVertically(-1)) {
+                    if (!this.canScrollVertically(1)) {
+                        for (i in 0 until this.childCount) {
+                            val view: View = this.getChildAt(i)
+                            val delta = oldYMove - rawY
+                            if (delta > 0) {
+                                if (oldYMove != rawY) {
+                                    isOverScrolling = true
+                                    view.translationY  = (delta * 0.50f) * -1
+                                }
                             }
                         }
+                        return@setOnTouchListener false
                     }
-                } else {
-                    for (i in 0 until this.childCount) {
-                        val view: View = this.getChildAt(i)
-                        val delta = rawY - oldYMove
-                        if (delta > 0) {
-                            view.translationY = 0f
+
+                    //verticalMovement up to down
+                    if (!this.canScrollVertically(-1)) {
+                        for (i in 0 until this.childCount) {
+                            val view: View = this.getChildAt(i)
+                            val delta = oldYMove - rawY
+                            if (delta < 0) {
+                                if (oldYMove != rawY) {
+                                    isOverScrolling = true
+                                    view.translationY  = (delta * 0.50f) * -1
+                                }
+                            }
                         }
+                        return@setOnTouchListener false
+
                     }
                 }
+
+
                 oldYMove = rawY
+                oldXMove = rawX
             }
             if (event.action == MotionEvent.ACTION_UP) {
 
                 for (i in 0 until this.childCount) {
                     val view: View = this.getChildAt(i)
-                    view.translationY = 0f
-                    if (isOverScrolling) {
-                        view.bouncingAnimation()
+
+                    if (isOverScrolling && isOverScrollingVertical) {
+                        view.translationYAnimation()
+                    } else if (isOverScrolling && !isOverScrollingVertical) {
+                        view.translationXAnimation()
                     }
+
                 }
                 if (isOverScrolling) {
                     isOverScrolling = false
                     oldYMove = 0f
+                    oldXMove = 0f
                 }
             }
             false
         }
     }
+
+
 }
 
-private fun View.bouncingAnimation() {
+private fun View.bouncingAnimationY() {
     val springAnimationY: SpringAnimation = this.let {
         SpringAnimation(it, DynamicAnimation.TRANSLATION_Y, it.translationY)
     }
 
-    springAnimationY.setMaxValue(500f)
+    springAnimationY.setMaxValue(200f)
 
-    val velocity = 1000f
+    val velocity = 750f
     springAnimationY.setStartVelocity(velocity)
-    springAnimationY.spring?.dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+    springAnimationY.spring?.dampingRatio = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY
     springAnimationY.spring?.stiffness = SpringForce.STIFFNESS_MEDIUM
 
     springAnimationY.start()
+}
+
+private fun View.bouncingAnimationX() {
+    val springAnimationX: SpringAnimation = this.let {
+        SpringAnimation(it, DynamicAnimation.TRANSLATION_X, it.translationX)
+    }
+
+    springAnimationX.setMaxValue(200f)
+
+    val velocity = 750f
+    springAnimationX.setStartVelocity(velocity)
+    springAnimationX.spring?.dampingRatio = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY
+    springAnimationX.spring?.stiffness = SpringForce.STIFFNESS_MEDIUM
+
+    springAnimationX.start()
+}
+
+private fun View.translationXAnimation() {
+    val objectAnimator = ObjectAnimator.ofFloat(this, View.TRANSLATION_X, this.translationX, 0f)
+    objectAnimator.duration = 180
+    objectAnimator.addListener(object : Animator.AnimatorListener {
+        override fun onAnimationRepeat(animation: Animator?) {}
+        override fun onAnimationEnd(animation: Animator?) {
+            this@translationXAnimation.bouncingAnimationX()
+        }
+
+        override fun onAnimationCancel(animation: Animator?) {}
+        override fun onAnimationStart(animation: Animator?) {}
+
+    })
+    objectAnimator.start()
+}
+
+private fun View.translationYAnimation() {
+    val objectAnimator = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, this.translationY, 0f)
+    objectAnimator.duration = 180
+    objectAnimator.addListener(object : Animator.AnimatorListener {
+        override fun onAnimationRepeat(animation: Animator?) {}
+        override fun onAnimationEnd(animation: Animator?) {
+            this@translationYAnimation.bouncingAnimationY()
+        }
+
+        override fun onAnimationCancel(animation: Animator?) {}
+        override fun onAnimationStart(animation: Animator?) {}
+
+    })
+    objectAnimator.start()
 }
