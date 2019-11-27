bf2956f8dc8171aa3824537c24c8f7fd6f7ed598
Ever <ever.solis@elaniin.com>
2019-11-27 17:03:07 -0600

    Bounce scroll fixed

--- app/src/main/java/com/example/bouncescrollbehaviour/MainActivity.kt
+++ app/src/main/java/com/example/bouncescrollbehaviour/MainActivity.kt
@@ -1,31 +1,26 @@
 package com.example.bouncescrollbehaviour
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val letters :  ArrayList<String> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        for(i in 0 until 40){
-            letters.add((i+10).toString())
+        buttonRecycler.setOnClickListener {
+            val intent = Intent(this,RecyclerViewExample::class.java)
+            startActivity(intent)
+
         }
 
-      val adapter =  DynamicAdapter(
-            layout =R.layout.item_recycler,
-            entries = letters,
-            action = fun(_,view,item,position){
-
-            })
-       // bouncingRecycler.setHasFixedSize(true)
-    //    bouncingRecycler.configureRecyclerGrid(4)
-      //  bouncingRecycler.adapter =  adapter
+        buttonScroll.setOnClickListener {
+           /* val intent = Intent(this,RecyclerViewExample::class.java)
+            startActivity(intent)*/
+        }
 
     }
-
-
 }
--- app/src/main/java/com/example/bouncescrollbehaviour/RecyclerViewExample.kt
+++ app/src/main/java/com/example/bouncescrollbehaviour/RecyclerViewExample.kt
@@ -2,30 +2,63 @@ package com.example.bouncescrollbehaviour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_recycler_view_example.*
+
 
 class RecyclerViewExample : AppCompatActivity() {
+
+    private val images: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view_example)
+        setData()
+
+        val adapter = DynamicAdapter(
+            layout = R.layout.item_recycler,
+            entries = images,
+            action = fun(_, view, item, position) {
+            })
+        verticalRecycler.setHasFixedSize(true)
+        verticalRecycler.configureRecyclerGrid(4)
+        verticalRecycler.adapter = adapter
+
+        horizontalRecycler.setHasFixedSize(true)
+        horizontalRecycler.configureRecycler()
+        horizontalRecycler.adapter = adapter
+
     }
 
-
-
-
-
-
-
-
-    private fun androidx.recyclerview.widget.RecyclerView.configureRecyclerGrid(elementPerRow: Int = 3): androidx.recyclerview.widget.RecyclerView {
-        itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
-        layoutManager = androidx.recyclerview.widget.GridLayoutManager(context,elementPerRow)
-        return this
+    private fun setData() {
+        for (i in 1 until 85) {
+            var imageName: String = "$i"
+            if (i  < 10) {
+                imageName = "00$i"
+            } else if (i  < 100) {
+                imageName = "0$i"
+            }
+            images.add("https://assets.pokemon.com/assets/cms2/img/pokedex/full/$imageName.png")
+        }
     }
 
-    private fun androidx.recyclerview.widget.RecyclerView.configureRecycler(): androidx.recyclerview.widget.RecyclerView {
-        itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
-        layoutManager = androidx.recyclerview.widget.LinearLayoutManager(context, androidx.recyclerview.widget.RecyclerView.HORIZONTAL, false)
-        return this
-    }
 }
+
+private fun androidx.recyclerview.widget.RecyclerView.configureRecyclerGrid(elementPerRow: Int = 3): androidx.recyclerview.widget.RecyclerView {
+    itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
+    layoutManager = androidx.recyclerview.widget.GridLayoutManager(context, elementPerRow)
+    return this
+}
+
+private fun androidx.recyclerview.widget.RecyclerView.configureRecycler(): androidx.recyclerview.widget.RecyclerView {
+    itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
+    layoutManager = androidx.recyclerview.widget.LinearLayoutManager(
+        context,
+        androidx.recyclerview.widget.RecyclerView.HORIZONTAL,
+        false
+    )
+    return this
+}
+
+
+
+
